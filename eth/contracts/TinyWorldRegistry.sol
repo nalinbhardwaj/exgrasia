dfae5558824ef7b45a9b68f832856a2e17f111e1
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2022-03-30 12:48:04 -0700

    Fix registry bugs, frontend realAddress fetching

--- eth/contracts/TinyWorldRegistry.sol
+++ eth/contracts/TinyWorldRegistry.sol
@@ -21,24 +21,26 @@ contract TinyWorldRegistry {
         require(proxyAddressToRealAddress[_proxyAddress] == address(0), "Real address already set");
         realAddressToProxyAddress[msg.sender] = _proxyAddress;
         proxyAddressToRealAddress[_proxyAddress] = msg.sender;
+        registeredRealAddresses.push(msg.sender);
     }
 
     // DELETE BEFORE DEPLOY
-    function dummySetProxyAddress(address[] memory realAddresses, address proxyAddress) public {
-        for (uint256 i = 0; i < realAddresses.length; i++) {
-            address realAddress = realAddresses[i];
-            require(
-                realAddressToProxyAddress[realAddress] == address(0),
-                "Proxy address already set"
-            );
-            require(
-                proxyAddressToRealAddress[proxyAddress] == address(0),
-                "Real address already set"
-            );
-            realAddressToProxyAddress[realAddress] = proxyAddress;
-            proxyAddressToRealAddress[proxyAddress] = realAddress;
-        }
-    }
+    // function dummySetProxyAddress(address[] memory realAddresses, address proxyAddress) public {
+    //     for (uint256 i = 0; i < realAddresses.length; i++) {
+    //         address realAddress = realAddresses[i];
+    //         require(
+    //             realAddressToProxyAddress[realAddress] == address(0),
+    //             "Proxy address already set"
+    //         );
+    //         require(
+    //             proxyAddressToRealAddress[proxyAddress] == address(0),
+    //             "Real address already set"
+    //         );
+    //         realAddressToProxyAddress[realAddress] = proxyAddress;
+    //         proxyAddressToRealAddress[proxyAddress] = realAddress;
+    //         registeredRealAddresses.push(realAddress);
+    //     }
+    // }
 
     function getProxyAddress(address _realAddress) public view returns (address) {
         return realAddressToProxyAddress[_realAddress];
